bae8effa25cc73a77657a0e256acdc463fb324cb
qwaszxlll <caleblin@mit.edu>
2014-10-14 05:39:53 -0400

    final timing tweak, should be pretty easy early on and increase in difficulty

--- Assets/scripts/Building.cs
+++ Assets/scripts/Building.cs
@@ -50,7 +50,7 @@ public class Building : MonoBehaviour {
 
         //update spawntime
 		timeLeft -= 1;
-		spawnTime = initSpawnTime - 1.8f * player.GetInfamy();
+		spawnTime = initSpawnTime - 2f * player.GetInfamy();
 
 		//spawn Champion
 		if (timeLeft < 0 && type != "farm"){
